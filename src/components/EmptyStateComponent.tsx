fed84cfa2dddc190275e37a946bc98251e6b6393
Hui Jian <huijian_cheng@hotmail.com>
2022-04-11 16:06:20 +0800

    Final changes for review

--- src/components/EmptyStateComponent.tsx
+++ src/components/EmptyStateComponent.tsx
@@ -1,33 +1,42 @@
 import React from 'react';
 import { Button, Card, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 interface EmptyStateComponentProps {
   message?: string;
   buttonText?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
-const style = {
-  position: 'relative' as 'relative',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #0F0',
-  boxShadow: 24,
-  p: 4,
-};
-
 const EmptyStateComponent: React.FC<EmptyStateComponentProps> = ({
   message,
   buttonText,
   onClick,
-}) => (
-  <div>
-    <Card sx={style}>
-      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-        {message}
-      </Typography>
-      <Button onClick={onClick}>{buttonText}</Button>
-    </Card>
-  </div>
-);
+}) => {
+  const theme = useTheme();
+
+  return (
+    <div>
+      <Card
+        sx={{
+          position: 'relative',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: 400,
+          bgcolor: theme.palette.dark.dark,
+          border: `1px solid${theme.palette.primary.main}`,
+          boxShadow: 24,
+          p: 4,
+          textAlign: 'center',
+        }}
+      >
+        <Typography sx={{ mt: 2, color: 'white' }}>{message}</Typography>
+        <Button variant="contained" sx={{ mt: 2 }} onClick={onClick}>
+          {buttonText}
+        </Button>
+      </Card>
+    </div>
+  );
+};
 
 export default EmptyStateComponent;
