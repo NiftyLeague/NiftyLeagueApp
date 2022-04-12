bcfe561a062d2cf4c7fcf5e715b407383f36e54c
Hui Jian <huijian_cheng@hotmail.com>
2022-04-12 22:49:53 +0800

    First push that also addresses NF-31

---
+++ src/components/cards/PopUpCard.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { useTheme } from '@mui/material/styles';
+
+interface PopUpCardProps {
+  title?: string;
+  content?: string;
+  uielement?: JSX.Element;
+}
+
+const PopUpCard: React.FC<PopUpCardProps> = ({ title, content, uielement }) => {
+  const [open, setOpen] = React.useState(true);
+  const handleClose = () => setOpen(false);
+  const theme = useTheme();
+
+  return (
+    <div>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            position: 'relative',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 800,
+            bgcolor: theme.palette.dark.dark,
+            border: `1px solid${theme.palette.primary.main}`,
+            boxShadow: 24,
+            p: 4,
+            textAlign: 'center',
+          }}
+        >
+          <Typography variant="h3">{title}</Typography>
+          <Typography sx={{ m: 2 }}>{content}</Typography>
+          {uielement}
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default PopUpCard;
--- src/pages/sample-page/index.tsx
+++ src/pages/sample-page/index.tsx
@@ -1,9 +1,9 @@
 // material-ui
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
 // project imports
 import MainCard from 'components/cards/MainCard';
-import EmptyStateModal from 'components/EmptyStateComponent';
+import PopUpCard from 'components/cards/PopUpCard';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
@@ -20,12 +20,26 @@ const SamplePage = () => (
         labours.
       </Typography>
     </MainCard>
-    <EmptyStateModal
+    {/* <EmptyStateModal
       message="You don't have anything here."
       buttonText="Buy stuff!"
     />
     <EmptyStateModal message="Is Wahyu the best?" buttonText="Yes!" />
-    <EmptyStateModal message="I love cats" buttonText="Me too!" />
+    <EmptyStateModal message="I love cats" buttonText="Me too!" /> */}
+    <PopUpCard
+      title="Desktop App"
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      uielement={
+        <>
+          <Button variant="contained" sx={{ m: 2 }}>
+            Get Windows
+          </Button>
+          <Button variant="contained" sx={{ m: 2 }}>
+            Get MacOS
+          </Button>
+        </>
+      }
+    />
   </>
 );
 
