9e8407deef179231b06ba5adc460436f0b1440f7
Chau Duong <chau.duong@leflair.vn>
2022-04-19 15:51:01 +0700

    feat: 🎸 use react-ga to tracking event and pageview

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,8 +5,12 @@ import MainRoutes from './MainRoutes';
 import LoginRoutes from './LoginRoutes';
 import AuthenticationRoutes from './AuthenticationRoutes';
 
+// hooks
+import useGoogleAnalytics from 'hooks/useGoogleAnalytics';
+
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
+  useGoogleAnalytics();
   return useRoutes([MainRoutes, LoginRoutes, AuthenticationRoutes]);
 }
