50484ca712cbc3ffde905663728edfd69ee3f470
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-04-23 11:19:19 +0100

    ⚡️  feat: add network provider and some types

---
+++ src/types/notify.ts
@@ -0,0 +1,27 @@
+import { providers, utils } from 'ethers';
+import {
+  EthereumTransactionLog,
+  EthereumTransactionData,
+  SDKError,
+} from 'bnc-sdk/dist/types/src/interfaces';
+import { EthereumRpcError, EthereumProviderError } from 'eth-rpc-errors';
+
+type Transaction =
+  | Promise<providers.TransactionResponse>
+  | utils.Deferrable<providers.TransactionRequest>;
+
+export type NotifyTransactionResult =
+  | EthereumTransactionLog
+  | EthereumTransactionData
+  | providers.TransactionReceipt;
+
+export type NotifyCallback = (res: NotifyTransactionResult) => void;
+
+export type Tx = (
+  tx: Transaction,
+  callback?: NotifyCallback,
+) => Promise<providers.TransactionResponse | null>;
+
+export type MetamaskError = EthereumRpcError<any> | EthereumProviderError<any>;
+
+export type NotifyError = SDKError | MetamaskError | Error | ErrorEvent;
