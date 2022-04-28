46bb9c78690ba1ae322de8a13587d178ce02a1f6
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-04-28 10:27:57 +0700

    ⚡️ feat: integrate comic page

--- src/constants/comics.ts
+++ src/constants/comics.ts
@@ -1,47 +1,54 @@
 import { Comic } from 'types/comic';
 
+import ComicP1 from 'assets/images/comics/page1.png';
+import ComicP2 from 'assets/images/comics/page2.png';
+import ComicP3 from 'assets/images/comics/page3.png';
+import ComicP4 from 'assets/images/comics/page4.png';
+import ComicP5 from 'assets/images/comics/page5.png';
+import ComicP6 from 'assets/images/comics/page6.png';
+
 const comics: Comic[] = [
   {
     id: 1,
     title: 'Comic',
     multiplier: 1.5,
     wearableName: '183x',
-    image: 'https://berrydashboard.io/static/media/profile-back-5.5f9f39de.png',
+    image: ComicP1,
   },
   {
     id: 2,
     title: 'Comic',
     multiplier: 1.5,
     wearableName: '235x',
-    image: 'https://berrydashboard.io/static/media/profile-back-7.d8ac257e.png',
+    image: ComicP2,
   },
   {
     id: 3,
     title: 'Comic',
     multiplier: 1.5,
     wearableName: '235x',
-    image: 'https://berrydashboard.io/static/media/profile-back-7.d8ac257e.png',
+    image: ComicP3,
   },
   {
     id: 4,
     title: 'Comic',
     multiplier: 1.5,
     wearableName: '235x',
-    image: 'https://berrydashboard.io/static/media/profile-back-7.d8ac257e.png',
+    image: ComicP4,
   },
   {
     id: 5,
     title: 'Comic',
     multiplier: 1.5,
     wearableName: '235x',
-    image: 'https://berrydashboard.io/static/media/profile-back-7.d8ac257e.png',
+    image: ComicP5,
   },
   {
     id: 6,
     title: 'Comic',
     multiplier: 1.5,
     wearableName: '235x',
-    image: 'https://berrydashboard.io/static/media/profile-back-7.d8ac257e.png',
+    image: ComicP6,
   },
 ];
 
