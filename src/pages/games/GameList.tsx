ee73cd822b68b08f0bb3a54cdbe6e8e5d78c40da
Chau Duong <chau.duong1192@gmail.com>
2022-04-28 17:26:36 +0700

    refactor: 💡 create new route for playing on game (#54)

---
+++ src/pages/games/GameList.tsx
@@ -0,0 +1,67 @@
+import { Grid, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import GameCard from 'components/cards/GameCard';
+import NiftySmashers from 'assets/images/gifs/nifty-smashers.gif';
+import { Dialog, DialogTrigger, DialogContent } from 'components/dialog';
+import Downloader from './Downloader';
+
+const GameList = () => {
+  const navigate = useNavigate();
+
+  const goToPlayOnGame = () => {
+    navigate('/play-on-game');
+  };
+
+  return (
+    <>
+      <Grid item sm={12} md={6} lg={4} xl={3}>
+        <GameCard
+          title="Nifty Smashers"
+          description="The first NFT brawler on the Ethereum blockchain!"
+          image={NiftySmashers}
+          onlineCounter={200}
+          actions={
+            <>
+              <Dialog>
+                <DialogTrigger>
+                  <Button
+                    variant="contained"
+                    fullWidth
+                    sx={{ minWidth: 80, flex: 1 }}
+                  >
+                    Play on Desktop
+                  </Button>
+                </DialogTrigger>
+                <DialogContent
+                  aria-labelledby="customized-dialog-title"
+                  dialogTitle="Nifty League Desktop"
+                >
+                  <Downloader />
+                </DialogContent>
+              </Dialog>
+              <Button
+                variant="outlined"
+                color="primary"
+                fullWidth
+                sx={{ minWidth: 80, flex: 1 }}
+                onClick={goToPlayOnGame}
+              >
+                Play on Web
+              </Button>
+            </>
+          }
+        />
+      </Grid>
+      <Grid item sm={12} md={6} lg={4} xl={3}>
+        <GameCard
+          title="Nifty Tennis"
+          description="The first ever NFT tennis game on the Ethereum blockchain"
+          isComingSoon
+          image={NiftySmashers}
+        />
+      </Grid>
+    </>
+  );
+};
+
+export default GameList;
--- src/pages/nifty-league-app/index.tsx
+++ src/pages/nifty-league-app/index.tsx
@@ -1,19 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Grid, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
-import GameCard from 'components/cards/GameCard';
 import { cardSpacing } from 'store/constant';
 import DegenCard from 'components/cards/DegenCard';
 import SectionSlider from 'components/sections/SectionSlider';
-import GameWithAuth, { smashersContext } from 'pages/games/GameWithAuth';
-import NiftySmashers from 'assets/images/gifs/nifty-smashers.gif';
-import { Dialog, DialogTrigger, DialogContent } from 'components/dialog';
-import Downloader from 'pages/games/Downloader';
 import { Degen } from 'types/degens';
 import { DEGEN_BASE_API_URL } from 'constants/url';
 import useFetch from 'hooks/useFetch';
 import SkeletonDegenPlaceholder from 'components/cards/Skeleton/DegenPlaceholder';
 import { v4 as uuidv4 } from 'uuid';
+import GameList from 'pages/games/GameList';
 
 const NiftyLeagueAppPage = () => {
   const [degens, setDegens] = useState<Degen[]>([]);
@@ -60,61 +56,7 @@ const NiftyLeagueAppPage = () => {
           flexWrap="wrap"
           spacing={cardSpacing}
         >
-          <Grid item sm={12} md={6} lg={4} xl={3}>
-            <GameCard
-              title="Nifty Smashers"
-              description="The first NFT brawler on the Ethereum blockchain!"
-              image={NiftySmashers}
-              onlineCounter={200}
-              actions={
-                <>
-                  <Dialog>
-                    <DialogTrigger>
-                      <Button
-                        variant="contained"
-                        fullWidth
-                        sx={{ minWidth: 80, flex: 1 }}
-                      >
-                        Play on Desktop
-                      </Button>
-                    </DialogTrigger>
-                    <DialogContent
-                      aria-labelledby="customized-dialog-title"
-                      dialogTitle="Nifty League Desktop"
-                    >
-                      <Downloader />
-                    </DialogContent>
-                  </Dialog>
-                  <Dialog>
-                    <DialogTrigger>
-                      <Button
-                        variant="outlined"
-                        color="primary"
-                        fullWidth
-                        sx={{ minWidth: 80, flex: 1 }}
-                      >
-                        Play on Web
-                      </Button>
-                    </DialogTrigger>
-                    <DialogContent
-                      aria-labelledby="customized-dialog-title"
-                      dialogTitle="Nifty League Web"
-                    >
-                      <GameWithAuth unityContext={smashersContext} />
-                    </DialogContent>
-                  </Dialog>
-                </>
-              }
-            />
-          </Grid>
-          <Grid item sm={12} md={6} lg={4} xl={3}>
-            <GameCard
-              title="Nifty Tennis"
-              description="The first ever NFT tennis game on the Ethereum blockchain"
-              isComingSoon
-              image={NiftySmashers}
-            />
-          </Grid>
+          <GameList />
         </Grid>
       </SectionSlider>
       <SectionSlider
