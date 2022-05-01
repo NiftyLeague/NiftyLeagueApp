06399f71edeae6dcdc0f4e2c9dd486a64b478693
Chau Duong <chau.duong1192@gmail.com>
2022-05-01 21:39:32 +0700

    refactor: 💡 handle game route (#66)

--- src/pages/games/PlayOnGame/GameWithAuth.tsx
+++ src/pages/games/PlayOnGame/GameWithAuth.tsx
@@ -5,7 +5,7 @@ import React, {
   useRef,
   useState,
 } from 'react';
-import { Button } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 import Unity, { UnityContext } from 'react-unity-webgl';
 import { NetworkContext } from 'NetworkProvider';
 import withVerification from 'components/Authentication';
@@ -111,35 +111,36 @@ const Game = ({
   }, [unityContext, onMouse, startAuthentication, getConfiguration]);
 
   const handleOnClickFullscreen = () => {
-    if ((window as any).unityInstance)
-      (window as any).unityInstance.setFullscreen(true);
-  };
-
-  const btnStyles = {
-    verticalAlign: 'top',
-    marginLeft: 8,
-    marginTop: 16,
-    background: '-webkit-linear-gradient(89deg, #620edf 0%, #5e72eb 100%)',
-    color: '#fff',
-    borderColor: '#6f6c6c',
+    (window as any).unityInstance.setFullscreen(true);
   };
 
   return (
     <>
       <Preloader ready={isLoaded} progress={progress} />
-      <Unity
-        className="game-canvas"
-        unityContext={unityContext}
-        style={{
-          width: 'calc(90vh * 1.33)',
-          height: '90vh',
-          marginTop: 5,
-          visibility: isLoaded ? 'visible' : 'hidden',
-        }}
-      />
-      <Button style={btnStyles} size="large" onClick={handleOnClickFullscreen}>
-        Fullscreen
-      </Button>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="flex-start"
+        width="100%"
+        gap={1}
+      >
+        <Unity
+          className="game-canvas"
+          unityContext={unityContext}
+          style={{
+            width: 'calc(89vh * 1.33)',
+            height: '89vh',
+            visibility: isLoaded ? 'visible' : 'hidden',
+          }}
+        />
+        <Button
+          variant="contained"
+          size="large"
+          onClick={handleOnClickFullscreen}
+        >
+          Fullscreen
+        </Button>
+      </Stack>
     </>
   );
 };
