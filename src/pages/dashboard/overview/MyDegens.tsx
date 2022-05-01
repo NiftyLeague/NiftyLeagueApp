5f11b1738ed7fba3ef3991cf1beb584cff878469
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-05-01 11:35:15 +0700

    ⚡️  feat: update my degens fetch data

--- src/pages/dashboard/overview/MyDegens.tsx
+++ src/pages/dashboard/overview/MyDegens.tsx
@@ -1,7 +1,21 @@
 import { Box, Button } from '@mui/material';
 import DegenCard from 'components/cards/DegenCard';
-import degens from 'constants/degens';
+import dummyData from 'constants/degens';
 import SectionSlider from 'components/sections/SectionSlider';
+import { useContext, useMemo } from 'react';
+import { NetworkContext } from 'NetworkProvider';
+import { Owner } from 'types/graph';
+import { OWNER_QUERY } from 'queries/OWNER_QUERY';
+import { CHARACTERS_SUBGRAPH_INTERVAL } from '../../../constants';
+import useFetch from 'hooks/useFetch';
+import { Degen } from 'types/degens';
+import {
+  DEGEN_BASE_API_URL,
+  MY_PROFILE_API_URL,
+  MY_RENTAL_API_URL,
+} from 'constants/url';
+import { useQuery } from '@apollo/client';
+import { Rentals } from 'types/rentals';
 
 interface MyDegensProps {
   onViewAllDegens?: React.MouseEventHandler<HTMLButtonElement>;
@@ -21,6 +35,56 @@ const BoxDegenStyles = {
 };
 
 const MyDegens = ({ onViewAllDegens }: MyDegensProps): JSX.Element => {
+  const authToken = window.localStorage.getItem('authentication-token');
+  const { address } = useContext(NetworkContext);
+
+  let headers;
+  if (authToken) {
+    headers = {
+      authorizationToken: authToken,
+    };
+  }
+
+  const { data: userDegens }: { loading: boolean; data?: { owner: Owner } } =
+    useQuery(OWNER_QUERY, {
+      pollInterval: CHARACTERS_SUBGRAPH_INTERVAL,
+      variables: { address: address?.toLowerCase() },
+      skip: !address,
+    });
+
+  const { data: userRentals } = useFetch<Rentals[]>(MY_RENTAL_API_URL, {
+    headers,
+  });
+
+  const { data: degensData } = useFetch<Degen[]>(
+    `${DEGEN_BASE_API_URL}/cache/rentals/rentables.json`,
+  );
+
+  const { data: profile } = useFetch(MY_PROFILE_API_URL, { headers });
+
+  const degens = useMemo(() => {
+    console.log('test');
+    if (userRentals && userRentals.length) {
+      const mapDegens = userRentals.map((rental) => {
+        console.log(degensData?.[rental.degen_id]);
+        return degensData?.[rental.degen_id];
+      });
+
+      return mapDegens;
+    }
+    return [];
+  }, [userRentals, degensData]);
+
+  // useEffect(() => {
+  //   if (userRentals?.length) {
+  //     const degens = characters.map((character) => {
+  //       const degen = degensData?.[character.id];
+  //       return degen;
+  //     });
+  //     setOwnedDegens(degens);
+  //   }
+  // }, [userRentals, degensData, ownedDegens]);
+  console.log({ userDegens, userRentals, degens, profile });
   const settings = {
     slidesToShow: 3,
     responsive: [
@@ -62,7 +126,7 @@ const MyDegens = ({ onViewAllDegens }: MyDegensProps): JSX.Element => {
         </Button>
       }
     >
-      {degens.map((degen) => (
+      {dummyData.map((degen) => (
         <Box sx={BoxDegenStyles} key={degen.id}>
           <DegenCard
             id={degen.id}
