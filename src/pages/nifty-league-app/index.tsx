22b592e2a7d81b66d63904bc86fa5e621bd9f7bb
Zachery Ng <zachery.ng@gmail.com>
2022-05-02 14:07:18 +0800

    style: update no. of popular degen cards per breakpoint and float left

--- src/pages/nifty-league-app/index.tsx
+++ src/pages/nifty-league-app/index.tsx
@@ -59,31 +59,25 @@ const NiftyLeagueAppPage = () => {
     slidesToShow: 5,
     responsive: [
       {
-        breakpoint: 1600,
-        settings: {
-          slidesToShow: 5,
-        },
-      },
-      {
-        breakpoint: 1200,
+        breakpoint: 1800,
         settings: {
           slidesToShow: 4,
         },
       },
       {
-        breakpoint: 900,
+        breakpoint: 1500,
         settings: {
           slidesToShow: 3,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 900,
         settings: {
           slidesToShow: 2,
         },
       },
       {
-        breakpoint: 400,
+        breakpoint: 600,
         settings: {
           slidesToShow: 1,
         },
