ee48e7d87a598793e1300044a2a85298e86e1425
Zachery Ng <zachery.ng@gmail.com>
2022-05-02 15:12:02 +0800

    refactor: update text of rent degen dialog

--- src/components/dialog/DegenDialog/RentDegenContentDialog.tsx
+++ src/components/dialog/DegenDialog/RentDegenContentDialog.tsx
@@ -6,7 +6,6 @@ import {
   FormControl,
   FormControlLabel,
   Grid,
-  Link,
   Radio,
   RadioGroup,
   Stack,
@@ -35,7 +34,7 @@ const RentDegenContentDialog = ({
   onClose,
 }: RentDegenContentDialogProps) => {
   const [agreement, setAgreement] = useState<boolean>(false);
-  const [rentFor, setRentFor] = useState<string>('scholar');
+  const [rentFor, setRentFor] = useState<string>('recruit');
   const [renameEnabled, setRenameEnabled] = useState<boolean>(false);
   const [ethAddress, setEthAddress] = useState<string>('');
   const [newDegenName, setNewDegenName] = useState<string>('');
@@ -120,8 +119,8 @@ const RentDegenContentDialog = ({
   );
 
   return (
-    <Grid container>
-      <Grid item xs={12} sm={12} md={6} sx={{ py: 1, px: 2 }}>
+    <Grid container sx={{ p: 2 }} spacing={3}>
+      <Grid item xs={12} sm={12} md={6}>
         <Stack direction="row" justifyContent="center">
           {degen && <DegenImage tokenId={degen.id} />}
         </Stack>
@@ -134,9 +133,9 @@ const RentDegenContentDialog = ({
           <Typography>What are you renting for?</Typography>
           <RadioGroup row onChange={handleChangeRentingFor} value={rentFor}>
             <FormControlLabel
-              value="scholar"
+              value="recruit"
               control={<Radio />}
-              label="Scholar"
+              label="Recruit"
             />
             <FormControlLabel
               value="myself"
@@ -146,7 +145,9 @@ const RentDegenContentDialog = ({
           </RadioGroup>
         </Stack>
         <Stack direction="column" alignItems="center" gap={2} sx={{ my: 2 }}>
-          <Typography>What is your scholars ETH wallet address?</Typography>
+          <Typography textAlign="center">
+            What is your recruits ETH wallet address?
+          </Typography>
           <FormControl fullWidth>
             <TextField
               placeholder="0xunkown"
@@ -161,7 +162,9 @@ const RentDegenContentDialog = ({
           </FormControl>
         </Stack>
         <Stack direction="column" alignItems="center" sx={{ my: 2 }}>
-          <Typography>Do you want to rename the degen?</Typography>
+          <Typography textAlign="center">
+            Do you want to rename the degen for your rental?
+          </Typography>
           <RadioGroup
             row
             onChange={handleChangeRenameDegen}
@@ -193,122 +196,126 @@ const RentDegenContentDialog = ({
           </Stack>
         )}
       </Grid>
-      <Grid
-        item
-        xs={12}
-        sm={12}
-        md={6}
-        sx={{ py: 1, px: 2, position: 'relative' }}
-      >
-        <Box
-          display="flex"
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Typography variant="h2">Rental Overview</Typography>
-        </Box>
-        <Stack gap={1} sx={{ my: 2 }}>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>Degen Being Rented</Typography>
-            <Typography color="gray">
-              {degen?.name || 'No Name Degen'}
-            </Typography>
-          </Stack>
-          <Stack direction="row" justifyContent="flex-end">
-            <Typography color="gray">Degen #{degen?.id}</Typography>
-          </Stack>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>Rental Term</Typography>
-            <Typography color="gray">1 week</Typography>
-          </Stack>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>Scholarship?</Typography>
-            <Typography color="gray">
-              {rentFor === 'scholar' ? 'Yes' : 'No'}
-            </Typography>
-          </Stack>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>Total Multipliers</Typography>
-            <Typography color="gray">{degen?.multiplier}x</Typography>
-          </Stack>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>Rental Queue</Typography>
-            <Typography color="gray">{degen?.rental_count}</Typography>
-          </Stack>
-        </Stack>
-        <Divider />
-        <Stack gap={1} sx={{ my: 2 }}>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>First Week Rental Cost:</Typography>
-            <Typography color="gray">{degen?.price} NFTL</Typography>
-          </Stack>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>Renews Daily After Week 1 at:</Typography>
-            <Typography color="gray">{degen?.price_daily} NFTL</Typography>
-          </Stack>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>Rental Passes Remaining</Typography>
-            <Typography color="gray">{rentalPassCount}</Typography>
-          </Stack>
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Typography variant="caption" color="gray">
-              Use a rental pass?
-            </Typography>
-            <RadioGroup
-              row
-              onChange={handleChangeUseRentalPass}
-              value={isUseRentalPass ? 'yes' : 'no'}
+      <Grid item xs={12} sm={12} md={6}>
+        <Stack sx={{ justifyContent: 'space-between', height: '100%' }} gap={2}>
+          <Stack>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="center"
             >
-              <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-              <FormControlLabel value="no" control={<Radio />} label="No" />
-            </RadioGroup>
-          </Stack>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>Renaming Fee</Typography>
-            <Typography color="gray">{renameFee} NFTL</Typography>
-          </Stack>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>Total Due Now</Typography>
-            <Typography color="gray">{`${
-              degen?.price || 0 + (renameEnabled ? renameFee : 0)
-            } NFTL`}</Typography>
-          </Stack>
-        </Stack>
-        <Divider />
-        <FormControl sx={{ mt: 1 }}>
-          <FormControlLabel
-            label={
-              <Typography variant="caption">
-                I have read the <Link href="#">terms & conditions</Link>{' '}
-                regarding disabling a rental
-              </Typography>
-            }
-            control={
-              <Checkbox
-                value={agreement}
-                onChange={(event) => setAgreement(event.target.checked)}
+              <Typography variant="h2">Rental Overview</Typography>
+            </Box>
+            <Stack gap={1} sx={{ my: 2 }}>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>Degen Being Rented</Typography>
+                <Typography color="gray">
+                  {degen?.name || 'No Name Degen'}
+                </Typography>
+              </Stack>
+              <Stack direction="row" justifyContent="flex-end">
+                <Typography color="gray">Degen #{degen?.id}</Typography>
+              </Stack>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>Rental Term</Typography>
+                <Typography color="gray">1 week</Typography>
+              </Stack>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>Sponsorship?</Typography>
+                <Typography color="gray">
+                  {rentFor === 'recruit' ? 'Yes' : 'No'}
+                </Typography>
+              </Stack>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>Total Multipliers</Typography>
+                <Typography color="gray">{degen?.multiplier}x</Typography>
+              </Stack>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>Rental Queue</Typography>
+                <Typography color="gray">{degen?.rental_count}</Typography>
+              </Stack>
+            </Stack>
+            <Divider />
+            <Stack gap={1} sx={{ my: 2 }}>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>First Week Rental Cost:</Typography>
+                <Typography color="gray">{degen?.price} NFTL</Typography>
+              </Stack>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>Renews Daily After Week 1 at:</Typography>
+                <Typography color="gray">{degen?.price_daily} NFTL</Typography>
+              </Stack>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>Rental Passes Remaining</Typography>
+                <Typography color="gray">{rentalPassCount}</Typography>
+              </Stack>
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Typography variant="caption" color="gray">
+                  Use a rental pass?
+                </Typography>
+                <RadioGroup
+                  row
+                  onChange={handleChangeUseRentalPass}
+                  value={isUseRentalPass ? 'yes' : 'no'}
+                >
+                  <FormControlLabel
+                    value="yes"
+                    control={<Radio />}
+                    label="Yes"
+                  />
+                  <FormControlLabel value="no" control={<Radio />} label="No" />
+                </RadioGroup>
+              </Stack>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>Renaming Fee</Typography>
+                <Typography color="gray">{renameFee} NFTL</Typography>
+              </Stack>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>Total Due Now</Typography>
+                <Typography color="gray">{`${
+                  degen?.price || 0 + (renameEnabled ? renameFee : 0)
+                } NFTL`}</Typography>
+              </Stack>
+            </Stack>
+            <Divider />
+            <FormControl sx={{ mt: 1 }}>
+              <FormControlLabel
+                label={
+                  <Typography variant="caption">
+                    I understand all the information regarding this rental and
+                    it&apos;s fees.
+                  </Typography>
+                }
+                control={
+                  <Checkbox
+                    value={agreement}
+                    onChange={(event) => setAgreement(event.target.checked)}
+                  />
+                }
               />
-            }
-          />
-        </FormControl>
-        <Stack direction="column" gap={1} width="100%" sx={{ mt: 2 }}>
-          <LoadingButton
-            variant="contained"
-            fullWidth
-            onClick={handleRent}
-            disabled={!agreement || Boolean(nameError) || Boolean(addressError)}
-            loading={loading}
-          >
-            Rent Degen
-          </LoadingButton>
-          <Button fullWidth onClick={onClose} disabled={loading}>
-            Close
-          </Button>
+            </FormControl>
+          </Stack>
+          <Stack direction="column" gap={1} width="100%">
+            <LoadingButton
+              variant="contained"
+              fullWidth
+              onClick={handleRent}
+              disabled={
+                !agreement || Boolean(nameError) || Boolean(addressError)
+              }
+              loading={loading}
+            >
+              Rent Degen
+            </LoadingButton>
+            <Button fullWidth onClick={onClose} disabled={loading}>
+              Close
+            </Button>
+          </Stack>
         </Stack>
       </Grid>
     </Grid>
