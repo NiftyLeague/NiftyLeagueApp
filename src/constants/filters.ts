62e9bea913dc7b3b4f370c9b708c8da142bfddd6
Chau Duong <chau.duong1192@gmail.com>
2022-05-11 18:21:27 +0700

    add tribe icon to filter

--- src/constants/filters.ts
+++ src/constants/filters.ts
@@ -1,4 +1,36 @@
-const tribes = ['Ape', 'Alien', 'Cat', 'Doge', 'Frog', 'Human'];
+import { ReactComponent as ApeIcon } from 'assets/images/tribe/ape.svg';
+import { ReactComponent as AlienIcon } from 'assets/images/tribe/alien.svg';
+import { ReactComponent as CatIcon } from 'assets/images/tribe/cat.svg';
+import { ReactComponent as DegoIcon } from 'assets/images/tribe/doge.svg';
+import { ReactComponent as FrogIcon } from 'assets/images/tribe/frog.svg';
+import { ReactComponent as HumanIcon } from 'assets/images/tribe/human.svg';
+
+const tribes = [
+  {
+    name: 'Ape',
+    icon: ApeIcon,
+  },
+  {
+    name: 'Alien',
+    icon: AlienIcon,
+  },
+  {
+    name: 'Cat',
+    icon: CatIcon,
+  },
+  {
+    name: 'Doge',
+    icon: DegoIcon,
+  },
+  {
+    name: 'Frog',
+    icon: FrogIcon,
+  },
+  {
+    name: 'Human',
+    icon: HumanIcon,
+  },
+];
 
 const backgrounds = ['Common', 'Rare', 'Meta', 'Legendary'];
 
