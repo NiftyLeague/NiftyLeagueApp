088723c5cd1f0696bc3f5b781cca5027a5f7880d
Chau Duong <chau.duong1192@gmail.com>
2022-05-18 22:28:35 +0700

    add profile component skeleton

--- src/pages/dashboard/gamer-profile/index.tsx
+++ src/pages/dashboard/gamer-profile/index.tsx
@@ -2,17 +2,18 @@ import { Grid, Stack, Typography } from '@mui/material';
 
 import usePlayerProfile from 'hooks/usePlayerProfile';
 
-// import SectionSlider from 'components/sections/SectionSlider';
+import SectionSlider from 'components/sections/SectionSlider';
 import ImageProfile from './ImageProfile';
 import RightInfo from './Stats/RightInfo';
 import LeftInfo from './Stats/LeftInfo';
 import TopInfo from './Stats/TopInfo';
+import ProfileTopSkeleton from './ProfileTopSkeleton';
 import EmptyState from 'components/EmptyState';
 
 import { sectionSpacing } from 'store/constant';
 
 const GamerProfile = (): JSX.Element => {
-  const { profile } = usePlayerProfile();
+  const { profile, error, profileLoading } = usePlayerProfile();
 
   const renderEmptyProfile = () => {
     return (
@@ -28,41 +29,57 @@ const GamerProfile = (): JSX.Element => {
     );
   };
 
-  const renderGamerProfile = () => {
+  const renderTopProfile = () => {
     return (
-      <Grid container gap={sectionSpacing}>
-        <Grid item container spacing={3}>
-          <Grid item xs={12} md={3.5}>
-            <ImageProfile />
-          </Grid>
-          <Grid item xs={12} md={8.5}>
-            <TopInfo total={profile?.stats?.total} />
-            <hr />
-            <Stack spacing={1}>
-              <Stack>
-                <Typography variant="h3" component="div">
-                  Nifty League Player Stats
-                </Typography>
-              </Stack>
-              <Stack direction="row" spacing={5}>
-                <LeftInfo total={profile?.stats?.total} />
-                <RightInfo />
-              </Stack>
-            </Stack>
-          </Grid>
+      <Grid item container spacing={3}>
+        <Grid item xs={12} md={3.5}>
+          <ImageProfile />
         </Grid>
-        {/* TODO: NF-249 */}
-        {/* <Stack gap={sectionSpacing}>
-          <SectionSlider
-            firstSection
-            title="Player Stats by Game"
-          ></SectionSlider>
-        </Stack> */}
+        <Grid item xs={12} md={8.5}>
+          <TopInfo total={profile?.stats?.total} />
+          <hr />
+          <Stack spacing={1}>
+            <Stack>
+              <Typography variant="h3" component="div">
+                Nifty League Player Stats
+              </Typography>
+            </Stack>
+            <Stack direction="row" spacing={5}>
+              <LeftInfo total={profile?.stats?.total} />
+              <RightInfo />
+            </Stack>
+          </Stack>
+        </Grid>
       </Grid>
     );
   };
 
-  return !profile ? renderEmptyProfile() : renderGamerProfile();
+  const renderBottomProfile = () => {
+    return (
+      <Stack gap={sectionSpacing}>
+        <SectionSlider
+          firstSection
+          title="Player Stats by Game"
+        ></SectionSlider>
+      </Stack>
+    );
+  };
+
+  const renderGamerProfile = () => {
+    return (
+      <>
+        {profile && renderTopProfile()}
+        {profileLoading && <ProfileTopSkeleton />}
+        {renderBottomProfile()}
+      </>
+    );
+  };
+  return (
+    <Grid container gap={sectionSpacing}>
+      {error && !profile && renderEmptyProfile()}
+      {renderGamerProfile()}
+    </Grid>
+  );
 };
 
 export default GamerProfile;
