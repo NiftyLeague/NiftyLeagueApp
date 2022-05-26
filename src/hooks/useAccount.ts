85174e047c185b6453773e219dafe5c235940c9f
Chau Duong <chau.duong1192@gmail.com>
2022-05-26 12:10:46 +0700

    refactor useGameBalance hooks

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,19 +1,35 @@
+import { useCallback, useEffect, useState } from 'react';
 import { GAMER_ACCOUNT_API } from 'constants/url';
 import { Account } from 'types/account';
-import useFetch from './useFetch';
 
-const useAllRentals = (): {
-  error?: Error;
-  account?: Account;
-  loadingAccount?: boolean;
-} => {
+const useAccount = (
+  refreshKey?: string | number,
+): { error: boolean; account: Account | undefined } => {
   const auth = window.localStorage.getItem('authentication-token');
-  let headers;
-  if (auth) headers = { authorizationToken: auth };
-  const { error, data, loading } = useFetch<Account>(GAMER_ACCOUNT_API, {
-    headers,
-  });
-  return { error, account: data, loadingAccount: loading };
+  const [account, setAccount] = useState<Account | undefined>(undefined);
+  const [error, setAccError] = useState(false);
+
+  const fetchAccount = useCallback(async () => {
+    if (auth) {
+      const result = await fetch(GAMER_ACCOUNT_API, {
+        headers: { authorizationToken: auth },
+      })
+        .then((res) => {
+          if (res.status === 404) setAccError(true);
+          return res.text();
+        })
+        .catch(() => {
+          setAccError(true);
+        });
+      if (result) setAccount(JSON.parse(result));
+    }
+  }, [auth]);
+
+  useEffect(() => {
+    if (auth) fetchAccount();
+  }, [auth, fetchAccount, refreshKey]);
+
+  return { error, account };
 };
 
-export default useAllRentals;
+export default useAccount;
