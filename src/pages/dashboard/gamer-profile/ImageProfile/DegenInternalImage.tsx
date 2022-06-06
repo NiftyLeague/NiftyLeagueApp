238b936659969cf05035ea088e5d7c33a7db733e
Chau Duong <chau.duong1192@gmail.com>
2022-06-06 10:41:41 +0700

    update avatar api (#228)

---
+++ src/pages/dashboard/gamer-profile/ImageProfile/DegenInternalImage.tsx
@@ -0,0 +1,24 @@
+import { memo } from 'react';
+import { CardMedia } from '@mui/material';
+import { Degen } from 'types/degens';
+
+const DegenInternalImage = memo(({ degen }: { degen: Degen }) => {
+  let setting: any = {
+    height: 320,
+    component: 'img',
+    image: degen?.url,
+    alt: degen?.name,
+  };
+  if (degen?.background === 'legendary') {
+    setting = {
+      ...setting,
+      component: 'video',
+      autoPlay: true,
+      loop: true,
+      muted: true,
+    };
+  }
+  return <CardMedia {...setting} />;
+});
+
+export default DegenInternalImage;
