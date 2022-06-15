1af4cac325a5b29303a7844f300cdd035b6a7f2b
Diego G <diego.garza@niftyleague.com>
2022-06-15 09:03:19 -0400

    Added buy arcade token flow on /games/wen-game route

--- src/pages/games/PlayOnGame/ArcadeTokensRequired.tsx
+++ src/pages/games/PlayOnGame/ArcadeTokensRequired.tsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Grid, Typography } from '@mui/material';
 import { gridSpacing } from 'store/constant';
+import BuyArcadeTokensDialog from 'components/dialog/BuyArcadeTokensDialog';
 
 const ArcadeTokensRequired: React.FC = () => {
+  const [openBuyAT, setOpenBuyAT] = useState(false);
+
   const handleBuyArcadeTokens = () => {
     // TODO: Integrate Buy Arcade Tokens here
+    setOpenBuyAT(true);
   };
 
   return (
-    <Grid container height="100%" alignItems="center" spacing={gridSpacing}>
-      <Grid item xs={12}>
-        <Grid container spacing={gridSpacing}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="div" textAlign="center">
-              Arcade Tokens Required
-            </Typography>
-          </Grid>
-          <Grid item xs={12} textAlign="center">
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={handleBuyArcadeTokens}
-            >
-              Buy Arcade Tokens
-            </Button>
+    <>
+      <Grid container height="100%" alignItems="center" spacing={gridSpacing}>
+        <Grid item xs={12}>
+          <Grid container spacing={gridSpacing}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="div" textAlign="center">
+                Arcade Tokens Required
+              </Typography>
+            </Grid>
+            <Grid item xs={12} textAlign="center">
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={handleBuyArcadeTokens}
+              >
+                Buy Arcade Tokens
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+      <BuyArcadeTokensDialog
+        open={openBuyAT}
+        onClose={() => setOpenBuyAT(false)}
+      />
+    </>
   );
 };
 
