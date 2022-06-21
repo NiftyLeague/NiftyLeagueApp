16128bd88eb3941bd27dc984d45164ce9e442f7e
Diego G <diego.garza@niftyleague.com>
2022-06-21 07:49:24 -0400

    Added Earning cap section on dashboard

--- src/pages/dashboard/overview/index.tsx
+++ src/pages/dashboard/overview/index.tsx
@@ -1,37 +1,66 @@
 import { Grid } from '@mui/material';
+import { ALL_RENTAL_API_URL } from 'constants/url';
+import useFetch from 'hooks/useFetch';
+import { useEffect, useState } from 'react';
+import { Rentals } from 'types/rentals';
+import EarningCap from './EarningCap';
 import MyComics from './MyComics';
 import MyDegens from './MyDegens';
 import MyNFTL from './MyNFTL';
 import MyRentals from './MyRentals';
 
-const DashboardOverview = (): JSX.Element => (
-  <Grid
-    container
-    flexDirection="row"
-    spacing={4}
-    sx={{ maxHeight: 'cacl(100vh - 100px)' }}
-  >
-    <Grid item xs={12} md={12} lg={5}>
-      <Grid container flexDirection="column" spacing={6}>
-        <Grid item xs={12}>
-          <MyNFTL />
+const DashboardOverview = (): JSX.Element => {
+  const authToken = window.localStorage.getItem('authentication-token');
+  let headers;
+  if (authToken) {
+    headers = {
+      authorizationToken: authToken,
+    };
+  }
+  const { data } = useFetch<Rentals[]>(ALL_RENTAL_API_URL, {
+    headers,
+  });
+
+  const [rentals, setRentals] = useState<Rentals[] | any>([]);
+
+  useEffect(() => {
+    if (data) {
+      setRentals(data);
+    }
+  }, [data]);
+
+  return (
+    <Grid
+      container
+      flexDirection="row"
+      spacing={4}
+      sx={{ maxHeight: 'cacl(100vh - 100px)' }}
+    >
+      <Grid item xs={12} md={12} lg={5}>
+        <Grid container flexDirection="column" spacing={6}>
+          <Grid item xs={12}>
+            <MyNFTL />
+          </Grid>
+          <Grid item xs={12}>
+            <EarningCap rentals={rentals} />
+          </Grid>
+          <Grid item xs={12}>
+            <MyRentals rentals={rentals} />
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <MyRentals />
+      </Grid>
+      <Grid item xs={12} md={12} lg={7}>
+        <Grid container flexDirection="column" spacing={6}>
+          <Grid item xs={12}>
+            <MyDegens />
+          </Grid>
+          <Grid item xs={12}>
+            <MyComics />
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
-    <Grid item xs={12} md={12} lg={7}>
-      <Grid container flexDirection="column" spacing={6}>
-        <Grid item xs={12}>
-          <MyDegens />
-        </Grid>
-        <Grid item xs={12}>
-          <MyComics />
-        </Grid>
-      </Grid>
-    </Grid>
-  </Grid>
-);
+  );
+};
 
 export default DashboardOverview;
--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -6,5 +6,12 @@ export const formatNumberToDisplay = (num?: number) =>
       })
     : 0;
 
-export const formatNumberToDisplayWithCommas = (num?: number) =>
-  num ? (Math.round(num * 100) / 100).toFixed(2) : 0;
+export const formatNumberToDisplayWithCommas = (num?: number) => {
+  if (num) {
+    const fixed = (Math.round(num * 100) / 100).toFixed(2);
+    const parts = fixed.toString().split('.');
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return parts.join('.');
+  }
+  return 0;
+};
