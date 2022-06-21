3d1d73e0505496301558d1939d040a0aaf3efa72
Chau Duong <chau.duong1192@gmail.com>
2022-06-21 17:33:09 +0700

    Update Profile Image & Username on Dashboard Left Pane

--- src/hooks/useGamerProfile/useGamerProfile.ts
+++ src/hooks/useGamerProfile/useGamerProfile.ts
@@ -6,6 +6,7 @@ const useGamerProfile = (): {
   error?: Error;
   profile?: Profile;
   loadingProfile?: boolean;
+  fetchUserProfile?: any;
 } => {
   const auth = window.localStorage.getItem('authentication-token');
   let headers;
@@ -13,7 +14,30 @@ const useGamerProfile = (): {
   const { error, data, loading } = useFetch<Profile>(GET_GAMER_PROFILE_API, {
     headers,
   });
-  return { error, profile: data, loadingProfile: loading };
+
+  const fetchUserProfile = async () => {
+    const res = await fetch(GET_GAMER_PROFILE_API, {
+      headers,
+    });
+    if (res.status === 404) {
+      throw Error('Not Found');
+    }
+    if (res.status === 200) {
+      const json = await res.json();
+      if (json.statusCode === 400) {
+        throw Error(json.body);
+      }
+      return json as Profile;
+    }
+    throw Error('Something wrong!');
+  };
+
+  return {
+    error,
+    profile: data,
+    loadingProfile: loading,
+    fetchUserProfile,
+  };
 };
 
 export default useGamerProfile;
