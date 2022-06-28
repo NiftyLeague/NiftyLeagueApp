5d9acffcdf7519f9703fdfc100dfe92acc29f6f5
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-06-28 11:19:19 +0100

    🔧 chore: add `beforeunloading` listener to remove active rental storage

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,36 @@ import ThemeCustomization from 'themes';
 
 // auth provider
 import { TokenProvider as AuthProvider } from 'contexts/TokenContext';
+import { useEffect } from 'react';
 
 // ==============================|| APP ||============================== //
 
-const App = () => (
-  <ThemeCustomization>
-    <Locales>
-      <NavigationScroll>
-        <AuthProvider>
-          <>
-            <Routes />
-            <Snackbar />
-          </>
-        </AuthProvider>
-      </NavigationScroll>
-    </Locales>
-  </ThemeCustomization>
-);
+const App = () => {
+  useEffect(() => {
+    window.addEventListener(
+      'beforeunload',
+      () => {
+        localStorage.removeItem('active_rental');
+      },
+      false,
+    );
+  });
+
+  return (
+    <ThemeCustomization>
+      <Locales>
+        <NavigationScroll>
+          <AuthProvider>
+            <>
+              <Routes />
+              <Snackbar />
+            </>
+          </AuthProvider>
+        </NavigationScroll>
+      </Locales>
+    </ThemeCustomization>
+  );
+};
 
 export default withLDProvider({
   clientSideID: process.env.REACT_APP_LAUNCHDARKLY_SDK_CLIENT!,
