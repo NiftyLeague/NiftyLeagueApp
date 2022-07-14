631c582579aecab940ee57ab2d3ee2ce0c6839be
nicolas <nicolas@niftyleague.com>
2022-07-14 09:36:54 -0400

    Move constants

--- src/components/dialog/DegenDialog/EquipDegenContentDialog.tsx
+++ src/components/dialog/DegenDialog/EquipDegenContentDialog.tsx
@@ -17,103 +17,26 @@ import { sendEvent } from 'utils/google-analytics';
 import { GOOGLE_ANALYTICS } from 'constants/google-analytics';
 import { COMIC_PURCHASE_URL } from 'constants/url';
 import { Degen } from 'types/degens';
-import { ReactComponent as CapeSlot } from 'assets/images/equips/cape-slot.svg';
-import { ReactComponent as CompanionSlot } from 'assets/images/equips/companion-slot.svg';
-import { ReactComponent as HaloSlot } from 'assets/images/equips/halo-slot.svg';
-import { ReactComponent as BatSlot } from 'assets/images/equips/bat-slot.svg';
-import { ReactComponent as CapeInventoryEmpty } from 'assets/images/equips/cape-inventory-empty.svg';
-import { ReactComponent as CompanionInventoryEmpty } from 'assets/images/equips/companion-inventory-empty.svg';
-import { ReactComponent as HaloInventoryEmpty } from 'assets/images/equips/halo-inventory-empty.svg';
-import { ReactComponent as BatInventoryEmpty } from 'assets/images/equips/bat-inventory-empty.svg';
-import CapeImage from 'assets/images/equips/cape.png';
-import CompanionImage from 'assets/images/equips/companion.png';
-import HaloImage from 'assets/images/equips/halo.png';
-import BreadBatImage from 'assets/images/equips/bread-bat.png';
-import DiamondBatImage from 'assets/images/equips/diamond-bat.png';
-import PurpleBatImage from 'assets/images/equips/purple-bat.png';
 import DegenImage from 'components/cards/DegenCard/DegenImage';
 import EmptyState from 'components/EmptyState';
+import {
+  getInventoryAnalyticsEventName,
+  getSlotAnalyticsEventName,
+  INVENTORIES,
+  SLOTS,
+} from 'constants/equips';
 
 export interface EquipDegenContentDialogProps {
   degen?: Degen;
   name?: string;
 }
 
-interface Slot {
-  name: string;
-  empty: JSX.Element;
-  filled?: JSX.Element;
-  filledArr?: JSX.Element[];
-}
-
-const slots: Slot[] = [
-  {
-    name: 'Back',
-    empty: <CapeSlot />,
-    filled: <img src={CapeImage} width={40} height={40} alt="Back" />,
-  },
-  {
-    name: 'Head',
-    empty: <HaloSlot />,
-    filled: <img src={HaloImage} width={40} height={40} alt="Head" />,
-  },
-  {
-    name: 'Pet',
-    empty: <CompanionSlot />,
-    filled: <img src={CompanionImage} width={40} height={40} alt="Pet" />,
-  },
-  {
-    name: 'Weapon',
-    empty: <BatSlot />,
-    filledArr: [
-      <img src={DiamondBatImage} width={40} height={40} alt="Diamond Bat" />,
-      <img src={PurpleBatImage} width={40} height={40} alt="Purple Bat" />,
-      <img src={BreadBatImage} width={40} height={40} alt="Bread Bat" />,
-    ],
-  },
-];
-
-const inventories: Slot[] = [
-  {
-    name: 'Cape',
-    empty: <CapeInventoryEmpty width={30} height={30} />,
-    filled: <img src={CapeImage} alt="Cape" className="inventory" />,
-  },
-  {
-    name: 'Halo',
-    empty: <HaloInventoryEmpty width={30} height={30} />,
-    filled: <img src={HaloImage} alt="Halo" className="inventory" />,
-  },
-  {
-    name: 'Companion',
-    empty: <CompanionInventoryEmpty width={30} height={30} />,
-    filled: <img src={CompanionImage} alt="Companion" className="inventory" />,
-  },
-  {
-    name: 'Diamond Bat',
-    empty: <BatInventoryEmpty width={30} height={30} />,
-    filled: (
-      <img src={DiamondBatImage} alt="Diamond Bat" className="inventory" />
-    ),
-  },
-  {
-    name: 'Purple Bat',
-    empty: <BatInventoryEmpty width={30} height={30} />,
-    filled: <img src={PurpleBatImage} alt="Purple Bat" className="inventory" />,
-  },
-  {
-    name: 'Bread Bat',
-    empty: <BatInventoryEmpty width={30} height={30} />,
-    filled: <img src={BreadBatImage} alt="Bread Bat" className="inventory" />,
-  },
-];
-
-// Hardcoded multipliers by inventories order
+// Hardcoded multipliers by INVENTORIES order
 // Should be given from BE later
 // each multiplier will be larger than 2
 const multipliers: number[] = [2, 3, 2, 3, 4, 2];
 
-// Hardcoded DEGEN equipped status by inventories order
+// Hardcoded DEGEN equipped status by INVENTORIES order
 // Should be given from BE later
 const initEquipped: boolean[] = new Array(6).fill(false);
 
@@ -189,33 +112,7 @@ const EquipDegenContentDialog = ({
       }
       newEquipped[index] = true;
       setPendingEquipped(newEquipped);
-      let eventName = '';
-      switch (inventories[index].name) {
-        case 'Cape':
-          eventName = GOOGLE_ANALYTICS.EVENTS.DEGEN_INVENTORY_CAPE_EQUIPPED;
-          break;
-        case 'Halo':
-          eventName = GOOGLE_ANALYTICS.EVENTS.DEGEN_INVENTORY_HALO_EQUIPPED;
-          break;
-        case 'Companion':
-          eventName =
-            GOOGLE_ANALYTICS.EVENTS.DEGEN_INVENTORY_COMPANION_EQUIPPED;
-          break;
-        case 'Diamond Bat':
-          eventName =
-            GOOGLE_ANALYTICS.EVENTS.DEGEN_INVENTORY_DIAMOND_BAT_EQUIPPED;
-          break;
-        case 'Purple Bat':
-          eventName =
-            GOOGLE_ANALYTICS.EVENTS.DEGEN_INVENTORY_PURPLE_BAT_EQUIPPED;
-          break;
-        case 'Bread Bat':
-          eventName =
-            GOOGLE_ANALYTICS.EVENTS.DEGEN_INVENTORY_BREAD_BAT_EQUIPPED;
-          break;
-        default:
-          break;
-      }
+      const eventName = getInventoryAnalyticsEventName(INVENTORIES[index].name);
       if (eventName) {
         sendEvent(eventName, GOOGLE_ANALYTICS.CATEGORIES.ECOMMERCE);
       }
@@ -234,23 +131,7 @@ const EquipDegenContentDialog = ({
         newEquipped[index] = false;
       }
       setPendingEquipped(newEquipped);
-      let eventName = '';
-      switch (slots[index].name) {
-        case 'Back':
-          eventName = GOOGLE_ANALYTICS.EVENTS.DEGEN_SLOT_BACK_UNEQUIPPED;
-          break;
-        case 'Head':
-          eventName = GOOGLE_ANALYTICS.EVENTS.DEGEN_SLOT_HEAD_UNEQUIPPED;
-          break;
-        case 'Pet':
-          eventName = GOOGLE_ANALYTICS.EVENTS.DEGEN_SLOT_PET_UNEQUIPPED;
-          break;
-        case 'Weapon':
-          eventName = GOOGLE_ANALYTICS.EVENTS.DEGEN_SLOT_WEAPON_UNEQUIPPED;
-          break;
-        default:
-          break;
-      }
+      const eventName = getSlotAnalyticsEventName(SLOTS[index].name);
       if (eventName) {
         sendEvent(eventName, GOOGLE_ANALYTICS.CATEGORIES.ECOMMERCE);
       }
@@ -291,16 +172,16 @@ const EquipDegenContentDialog = ({
     (index: number) => {
       if (index < 3) {
         return pendingEquipped[index]
-          ? slots[index].filled
-          : slots[index].empty;
+          ? SLOTS[index].filled
+          : SLOTS[index].empty;
       }
       const slicedArr = pendingEquipped.slice(3);
       const equippedBatIndex = slicedArr.findIndex((item) => !!item);
-      const filledArr = slots[index].filledArr;
+      const filledArr = SLOTS[index].filledArr;
       if (equippedBatIndex >= 0 && filledArr) {
         return filledArr[equippedBatIndex];
       } else {
-        return slots[index].empty;
+        return SLOTS[index].empty;
       }
     },
     [pendingEquipped],
@@ -402,7 +283,7 @@ const EquipDegenContentDialog = ({
             SLOTS
           </Typography>
           <Stack rowGap={3}>
-            {slots.map((slot, index) => (
+            {SLOTS.map((slot, index) => (
               <Box key={slot.name} width={40} height={40} position="relative">
                 {getSlotImage(index)}
                 {isEquippedSlot(index) && (
@@ -484,7 +365,7 @@ const EquipDegenContentDialog = ({
             INVENTORY
           </Typography>
           <Stack rowGap={1.25}>
-            {inventories.map((inventory, index) => (
+            {INVENTORIES.map((inventory, index) => (
               <Box
                 key={inventory.name}
                 width={30}
