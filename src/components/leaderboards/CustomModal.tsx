e0a3006e60b9b29d1e77a1375b0e186c1c0e4b0a
heyradcode <betterthanbestest@gmail.com>
2022-07-25 12:23:13 -0700

    chore: more fixes and cleanups

--- src/components/leaderboards/CustomModal.tsx
+++ src/components/leaderboards/CustomModal.tsx
@@ -18,16 +18,16 @@ const StyledModal = styled(ModalUnstyled)`
   justify-content: center;
 `;
 
-// const Backdrop = styled('div')`
-//   z-index: -1;
-//   position: fixed;
-//   right: 0;
-//   bottom: 0;
-//   top: 0;
-//   left: 0;
-//   background-color: rgba(0, 0, 0, 0.5);
-//   -webkit-tap-highlight-color: transparent;
-// `;
+const Backdrop = styled('div')`
+  z-index: -1;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  -webkit-tap-highlight-color: transparent;
+`;
 
 const style = (theme) => ({
   width: '67.5vh',
--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,4 +1,3 @@
 // action - account reducer
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
-export const FIREBASE_STATE_CHANGED = 'FIREBASE_STATE_CHANGED';
