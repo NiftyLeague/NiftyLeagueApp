925718f0895f3057b43ac12d275e02703d7827f6
heyradcode <betterthanbestest@gmail.com>
2022-08-04 11:36:26 -0700

    chore: kill all cap in Gawx description

--- src/pages/games/MiniGameList.tsx
+++ src/pages/games/MiniGameList.tsx
@@ -59,7 +59,7 @@ const MiniGameList: React.FC = () => {
         <GameCard
           title="Mt. Gawx"
           required=" "
-          description={`Hearing the DEGENs' desperate pleas to spend their hard-earned NFTL and with bigger sinks still under his development, Satoshi suggests the DEGENs climb to the top of the MT. GAWX volcano to offer their NFTL sacrifices to the fiery depths to see who might burn the most, and to discover whether the rumors of Rugman offering interesting rewards to burners are true.\n\nStrange thing is, every time they lob in NFTL, it's almost as if the volcano's… responding.\n\nCould the fabled 7th tribe be waking up from their centuries-long slumber, deep in the caves where Rugman resides?`}
+          description={`Hearing the DEGENs' desperate pleas to spend their hard-earned NFTL and with bigger sinks still under his development, Satoshi suggests the DEGENs climb to the top of the MT. Gawx volcano to offer their NFTL sacrifices to the fiery depths to see who might burn the most, and to discover whether the rumors of Rugman offering interesting rewards to burners are true.\n\nStrange thing is, every time they lob in NFTL, it's almost as if the volcano's… responding.\n\nCould the fabled 7th tribe be waking up from their centuries-long slumber, deep in the caves where Rugman resides?`}
           showMore={true}
           image={MtGawxThumbnail}
           autoHeight={true}
