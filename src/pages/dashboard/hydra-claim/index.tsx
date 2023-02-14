00bd5e8f62ef28da19b03b68d94afc802f8f29c1
NiftyAndy <andy@niftyleague.com>
2023-02-13 23:30:07 -0400

    Refactor main section moving to own component

--- src/pages/dashboard/hydra-claim/index.tsx
+++ src/pages/dashboard/hydra-claim/index.tsx
@@ -2,18 +2,8 @@
 import { useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import isEmpty from 'lodash/isEmpty';
 import { useSearchParams } from 'react-router-dom';
-import { Box, Button, Dialog, Grid, IconButton, Stack } from '@mui/material';
-import {
-  ArrowBackIosNew,
-  ArrowForwardIos,
-  Clear,
-  Whatshot,
-} from '@mui/icons-material';
-import xor from 'lodash/xor';
+import { Dialog, Stack } from '@mui/material';
 
-import DegenCard from 'components/cards/DegenCard';
-import DegenImage from 'components/cards/DegenCard/DegenImage';
-import SkeletonDegenPlaceholder from 'components/cards/Skeleton/DegenPlaceholder';
 import DegensFilter from 'components/extended/DegensFilter';
 import DEFAULT_STATIC_FILTER from 'components/extended/DegensFilter/constants';
 import {
@@ -21,22 +11,16 @@ import {
   updateFilterValue,
 } from 'components/extended/DegensFilter/utils';
 import CollapsibleSidebarLayout from 'components/layout/CollapsibleSidebarLayout';
-import SectionTitle from 'components/sections/SectionTitle';
-import { DEGEN_BASE_API_URL, DEGEN_OPENSEA_URL } from 'constants/url';
+import { DEGEN_BASE_API_URL } from 'constants/url';
+import { DAO_BURN_ADDY } from 'constants/addresses';
 import useFetch from 'hooks/useFetch';
 import { DegenFilter } from 'types/degenFilter';
 import { Degen } from 'types/degens';
-import { v4 as uuidv4 } from 'uuid';
 import NetworkContext from 'contexts/NetworkContext';
-import EmptyState from 'components/EmptyState';
 import BalanceContext from 'contexts/BalanceContext';
 import DegensTopNav from 'components/extended/DegensTopNav';
 import BurnDegensDialog from './dialogs/BurnDegensDialog';
-import { DAO_BURN_ADDY } from 'constants/addresses';
-
-const handleBuyDegen = () => {
-  window.open(DEGEN_OPENSEA_URL, '_blank');
-};
+import MainSection from './MainSection';
 
 const DashboardHydraClaimPage = (): JSX.Element => {
   const { address } = useContext(NetworkContext);
@@ -147,34 +131,6 @@ const DashboardHydraClaimPage = (): JSX.Element => {
     [populatedDegens.length, filters],
   );
 
-  const handleSelectDegen = useCallback(
-    (degen: Degen) => {
-      // xor creates an array of unique values that is the symmetric difference of the given arrays
-      const newSelectedDegens = xor(selectedDegens, [degen]);
-      setSelectedDegens(newSelectedDegens);
-    },
-    [selectedDegens],
-  );
-
-  const isGridView = layoutMode === 'gridView';
-
-  const renderSkeletonItem = useCallback(
-    () => (
-      <Grid
-        item
-        xs={isGridView ? 12 : 6}
-        sm={isGridView ? 6 : 4}
-        md={isGridView ? 4 : 3}
-        lg={isGridView ? (isDrawerOpen ? 4 : 3) : isDrawerOpen ? 3 : 2.4}
-        xl={isGridView ? 3 : 2}
-        key={uuidv4()}
-      >
-        <SkeletonDegenPlaceholder size={isGridView ? 'normal' : 'small'} />
-      </Grid>
-    ),
-    [isDrawerOpen, isGridView],
-  );
-
   const renderDrawer = useCallback(
     () => (
       <DegensFilter
@@ -187,143 +143,32 @@ const DashboardHydraClaimPage = (): JSX.Element => {
     [defaultValues, handleFilter, searchTerm],
   );
 
-  const renderDegen = useCallback(
-    (degen: Degen) => (
-      <Grid
-        key={degen.id}
-        item
-        xs={isGridView ? 12 : 6}
-        sm={isGridView ? 6 : 4}
-        md={isGridView ? 4 : 3}
-        lg={isGridView ? (isDrawerOpen ? 4 : 3) : isDrawerOpen ? 3 : 2.4}
-        xl={isGridView ? 3 : 2}
-      >
-        <DegenCard
-          degen={degen}
-          isSelectableDegen
-          isSelected={selectedDegens.includes(degen)}
-          isSelectionDisabled={
-            address.toLowerCase() !== DAO_BURN_ADDY.toLowerCase() &&
-            selectedDegens.length === 8
-          }
-          onClickSelect={() => handleSelectDegen(degen)}
-          size={isGridView ? 'normal' : 'small'}
-        />
-      </Grid>
-    ),
-    [address, isDrawerOpen, handleSelectDegen, isGridView, selectedDegens],
-  );
-
-  const renderDegenImage = useCallback(
-    (degen: Degen) => (
-      <Box
-        onClick={() => handleSelectDegen(degen)}
-        sx={{
-          cursor: 'pointer',
-          position: 'relative',
-          '&:hover .MuiSvgIcon-root': { display: 'block' },
-        }}
-      >
-        <DegenImage tokenId={degen.id} sx={{ height: 65, width: 65 }} />
-        <Clear
-          sx={{
-            display: 'none',
-            position: 'absolute',
-            margin: 'auto',
-            top: '15px',
-            left: '15px',
-            fontSize: '42px',
-          }}
-        />
-      </Box>
-    ),
-    [handleSelectDegen],
-  );
-
   const renderMain = useCallback(
     () => (
-      <Stack gap={1.5}>
-        {/* Main Grid title */}
-        <SectionTitle firstSection>
-          <Stack direction="row" alignItems="center" gap={1}>
-            <IconButton
-              onClick={() => setIsDrawerOpen(!isDrawerOpen)}
-              size="small"
-            >
-              {isDrawerOpen ? <ArrowBackIosNew /> : <ArrowForwardIos />}
-            </IconButton>
-            {filteredData.length} Degens
-          </Stack>
-        </SectionTitle>
-        {/* Main grid content */}
-        <Grid container spacing={2} mt={-4.5} mb={5}>
-          {loading || !address ? (
-            [...Array(12)].map(renderSkeletonItem)
-          ) : filteredData.length ? (
-            filteredData.map(renderDegen)
-          ) : !characters?.length ? (
-            <EmptyState
-              message="No DEGENs found. Please check your address or go purchase a degen if you have not done so already!"
-              buttonText="Buy some DEGENs"
-              onClick={handleBuyDegen}
-            />
-          ) : null}
-        </Grid>
-        <Box
-          sx={{
-            position: 'fixed',
-            top: 'auto',
-            bottom: 0,
-            height: 100,
-            minWidth: `calc(100% - ${isDrawerOpen ? 732 : 386}px)`,
-            backgroundColor: '#1E2023',
-            border: '1px solid #d5d9e9',
-            borderRadius: '15px 15px 0 0',
-            zIndex: 1100,
-            color: '#d5d9e9',
-            boxShadow:
-              '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)',
-            transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-          }}
-        >
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
-            gap={1}
-            mx={2}
-            height="100%"
-          >
-            <Stack direction="row" spacing={1} my={'auto'}>
-              {selectedDegens.length
-                ? selectedDegens.map(renderDegenImage)
-                : null}
-            </Stack>
-            <Stack direction="column" gap={1} alignItems="center" width="25%">
-              {`${selectedDegens.length} DEGENs Selected`}
-              <Button
-                onClick={() => setBurnDialogOpen(true)}
-                variant="contained"
-                fullWidth
-                endIcon={<Whatshot />}
-                disabled={incorrectDegenSelection}
-              >
-                {incorrectDegenSelection ? 'Select 8 DEGENs' : 'Burn'}
-              </Button>
-            </Stack>
-          </Stack>
-        </Box>
-      </Stack>
+      <MainSection
+        characters={characters}
+        filteredData={filteredData}
+        incorrectDegenSelection={incorrectDegenSelection}
+        isDrawerOpen={isDrawerOpen}
+        layoutMode={layoutMode}
+        loading={loading}
+        selectedDegens={selectedDegens}
+        setBurnDialogOpen={setBurnDialogOpen}
+        setIsDrawerOpen={setIsDrawerOpen}
+        setSelectedDegens={setSelectedDegens}
+      />
     ),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [
+      characters,
+      filteredData,
+      incorrectDegenSelection,
       isDrawerOpen,
-      filteredData.length,
+      layoutMode,
       loading,
-      address,
-      renderSkeletonItem,
-      renderDegen,
-      characters?.length,
+      selectedDegens,
+      setBurnDialogOpen,
+      setIsDrawerOpen,
+      setSelectedDegens,
     ],
   );
 
@@ -347,7 +192,6 @@ const DashboardHydraClaimPage = (): JSX.Element => {
           renderMain={renderMain}
         />
       </Stack>
-
       <Dialog open={burnDialogOpen} onClose={() => setBurnDialogOpen(false)}>
         <BurnDegensDialog
           selectedDegens={selectedDegens}
