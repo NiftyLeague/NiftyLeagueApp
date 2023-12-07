f07b093f2b33ad424db5dee4b1492fa53421e77b
NiftyAndy <andy@niftyleague.com>
2023-12-06 21:38:24 -0400

    Upgrade to latest Unity Webgl package

--- src/pages/games/PlayOnGame/CryptoWinterGame.tsx
+++ src/pages/games/PlayOnGame/CryptoWinterGame.tsx
@@ -1,23 +1,25 @@
-import { UnityContext } from 'react-unity-webgl';
+import { useUnityContext } from 'react-unity-webgl';
 import GameWithAuth from './GameWithAuth';
 
 const baseUrl = process.env.REACT_APP_UNITY_CRYPTO_WINTER_BASE_URL as string;
 const buildVersion = process.env
   .REACT_APP_UNITY_CRYPTO_WINTER_BASE_VERSION as string;
 
-export const cryptoWinterContext = new UnityContext({
-  loaderUrl: `${baseUrl}/Build/${buildVersion}.loader.js`,
-  dataUrl: `${baseUrl}/Build/${buildVersion}.data.br`,
-  frameworkUrl: `${baseUrl}/Build/${buildVersion}.framework.js.br`,
-  codeUrl: `${baseUrl}/Build/${buildVersion}.wasm.br`,
-  streamingAssetsUrl: `${baseUrl}/StreamingAssets`,
-  companyName: 'NiftyLeague',
-  productName: 'CryptoWinter',
-  productVersion: buildVersion,
-});
+const CryptoWinterGame = () => {
+  const cryptoWinterContext = useUnityContext({
+    loaderUrl: `${baseUrl}/Build/${buildVersion}.loader.js`,
+    dataUrl: `${baseUrl}/Build/${buildVersion}.data.br`,
+    frameworkUrl: `${baseUrl}/Build/${buildVersion}.framework.js.br`,
+    codeUrl: `${baseUrl}/Build/${buildVersion}.wasm.br`,
+    streamingAssetsUrl: `${baseUrl}/StreamingAssets`,
+    companyName: 'NiftyLeague',
+    productName: 'CryptoWinter',
+    productVersion: buildVersion,
+  });
 
-const CryptoWinterGame = () => (
-  <GameWithAuth unityContext={cryptoWinterContext} arcadeTokenRequired />
-);
+  return (
+    <GameWithAuth unityContext={cryptoWinterContext} arcadeTokenRequired />
+  );
+};
 
 export default CryptoWinterGame;
--- src/pages/games/PlayOnGame/SmashersGame.tsx
+++ src/pages/games/PlayOnGame/SmashersGame.tsx
@@ -1,4 +1,4 @@
-import { UnityContext } from 'react-unity-webgl';
+import { useUnityContext } from 'react-unity-webgl';
 import GameWithAuth from './GameWithAuth';
 
 const smashersBaseUrl = process.env.REACT_APP_UNITY_SMASHERS_BASE_URL as string;
@@ -7,23 +7,25 @@ const smashersBuildVersion = process.env
 
 const useCompressed = process.env.REACT_APP_UNITY_USE_COMPRESSED !== 'false';
 
-export const smashersContext = new UnityContext({
-  loaderUrl: `${smashersBaseUrl}/Build/${smashersBuildVersion}.loader.js`,
-  dataUrl: `${smashersBaseUrl}/Build/${smashersBuildVersion}.data${
-    useCompressed ? '.br' : ''
-  }`,
-  frameworkUrl: `${smashersBaseUrl}/Build/${smashersBuildVersion}.framework.js${
-    useCompressed ? '.br' : ''
-  }`,
-  codeUrl: `${smashersBaseUrl}/Build/${smashersBuildVersion}.wasm${
-    useCompressed ? '.br' : ''
-  }`,
-  streamingAssetsUrl: `${smashersBaseUrl}/StreamingAssets`,
-  companyName: 'NiftyLeague',
-  productName: 'NiftySmashers',
-  productVersion: smashersBuildVersion,
-});
+const SmashersGame = () => {
+  const smashersContext = useUnityContext({
+    loaderUrl: `${smashersBaseUrl}/Build/${smashersBuildVersion}.loader.js`,
+    dataUrl: `${smashersBaseUrl}/Build/${smashersBuildVersion}.data${
+      useCompressed ? '.br' : ''
+    }`,
+    frameworkUrl: `${smashersBaseUrl}/Build/${smashersBuildVersion}.framework.js${
+      useCompressed ? '.br' : ''
+    }`,
+    codeUrl: `${smashersBaseUrl}/Build/${smashersBuildVersion}.wasm${
+      useCompressed ? '.br' : ''
+    }`,
+    streamingAssetsUrl: `${smashersBaseUrl}/StreamingAssets`,
+    companyName: 'NiftyLeague',
+    productName: 'NiftySmashers',
+    productVersion: smashersBuildVersion,
+  });
 
-const SmashersGame = () => <GameWithAuth unityContext={smashersContext} />;
+  return <GameWithAuth unityContext={smashersContext} />;
+};
 
 export default SmashersGame;
